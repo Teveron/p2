efc8140535554548ad42266978d0ad73a81013cc
Teveron <46807566+Teveron@users.noreply.github.com>
2025-11-27 01:52:09 -0500

    Created build.sql
    Created build.sql
    Added a schema diagram

---
+++ Pj2_14_build.sql
@@ -0,0 +1,9 @@
+-- Drop database to reset
+DROP DATABASE IF EXISTS database_name;
+
+-- Create new database
+CREATE DATABASE Bellini;
+
+--CREATE SCHEMA schema_name;
+
+CREATE TABLE CourseCatalog2022
